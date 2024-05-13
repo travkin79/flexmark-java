cd53f5941f3cb802244deabd4e218cf069d17e42
Dietrich Travkin <dietrich.travkin@solunar.de>
2024-05-13 17:33:53 +0200

    Add copyright notice in source code

--- flexmark-ext-plantuml/src/main/java/com/vladsch/flexmark/ext/plantuml/PlantUmlBlockMarker.java
+++ flexmark-ext-plantuml/src/main/java/com/vladsch/flexmark/ext/plantuml/PlantUmlBlockMarker.java
@@ -1,3 +1,12 @@
+/*
+ * This work is made available under the terms of the Eclipse Public License (EPL) Version 1.0.
+ * The EPL 1.0 accompanies this distribution.
+ * 
+ * You may obtain a copy of the License at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ * 
+ * Copyright © 2022-2024 Advantest Europe GmbH. All rights reserved.
+ */
 package com.vladsch.flexmark.ext.plantuml;
 
 public enum PlantUmlBlockMarker {
